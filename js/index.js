4eb13f2141bbcce3ae6699e601a964d48963ac07
tommygebru <gebru.t@gmail.com>
2016-10-17 19:17:56 -0700

    Master: Beautify the js script and remove unused functions.

--- js/index.js
+++ js/index.js
@@ -1,30 +1,14 @@
 $(document).ready(function() {
 
-  $('.social li:nth-child(1)').mouseenter(function() {
-    $('.hint').empty().css('color', 'inherit').html("<h4><span>+ </span> Contribute</h4>");
-  });
-  $('.social li:nth-child(2)').mouseenter(function() {
-    $('.hint').empty().css('color', '#55acee').html("<h4>Tweet my work</h4>");
-  });
-  $('.social li:nth-child(3)').mouseenter(function() {
-    $('.hint').empty().css('color', '#dd4b39').html("<h4>Lets Talk</h4>");
-  });
-  $('.social').mouseleave(function() {
-    $('.hint').css('color', '#efefef');
-  });
+    $(".grad").each(function() {
 
-  
-  
-  
-  $(".grad").each(function() {
+        var rgb = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
+        //console.log(rgb + " rgb");
+        var color1 = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+        //console.log(color1 + " color1");
+        var color2 = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+        //console.log(color2 + " color2");
 
-    var rgb = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
-    console.log(rgb + " rgb");
-    var color1 = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
-    console.log(color1 + " color1");
-    var color2 = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
-    console.log(color2 + " color2");
-
-    $(this).css("background", "linear-gradient(" + color1 + "," + color2 + ")");
-  });
+        $(this).css("background", "linear-gradient(" + color1 + "," + color2 + ")");
+    });
 });
