43dd6d9b4231c68a2eb03b1493f791dd4258dda2
tommygebru <gebru.t@gmail.com>
2016-10-19 12:34:52 -0700

    Master: change gradient color and navigation color.

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 
     $(".grad").each(function() {
 
-        var rgb = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
+        var rgb = [Math.floor(Math.random() * 250), Math.floor(Math.random() * 250), Math.floor(Math.random() * 250)];
         //console.log(rgb + " rgb");
-        var color1 = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+        var color1 = 'rgb(' + Math.floor(Math.random() * 250) + ',' + Math.floor(Math.random() * 250) + ',' + Math.floor(Math.random() * 250) + ')';
         //console.log(color1 + " color1");
-        var color2 = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+        var color2 = 'rgb(' + Math.floor(Math.random() * 250) + ',' + Math.floor(Math.random() * 250) + ',' + Math.floor(Math.random() * 250) + ')';
         //console.log(color2 + " color2");
 
         $(this).css("background", "linear-gradient(" + color1 + "," + color2 + ")");
